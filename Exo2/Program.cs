c1631f6718befbbc2360b412be2d5e4e0822ac89
Singfield <rigaudlouis98@gmail.com>
2020-09-22 13:36:06 +0200

    add struct article

---
+++ Exo2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Exo2.models;
+
+namespace Exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Article article = new Article("Article1",2f, 3f);
+
+            article.Afficher();
+            article.Ajouter();
+            article.Afficher();
+            article.Retirer();
+            article.Afficher();
+        }
+    }
+}
