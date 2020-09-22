5c784e2156293eb063c1dadf18496a3a8e0dd8cb
Singfield <rigaudlouis98@gmail.com>
2020-09-22 23:10:01 +0200

    add exo3 qui n'a pas voulu etre upoad thx vs de merde

---
+++ Exo3/models/Article.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exo3.models
+{
+
+    public enum typeArticles
+    {
+        Droguerie,
+        alimentaire,
+        equipement,
+        habillement,
+        loisir
+
+    }
+    struct Article
+    {
+        public string nom { get; set; }
+        public float prix { get; set; }
+
+        public float quantite { get; set; }
+
+        public typeArticles typeArticle;
+
+        public Article(string nom, float prix, float quantite, typeArticles typeArticle)
+        {
+            this.typeArticle=typeArticle;
+            this.nom = nom;
+            this.prix = prix;
+            this.quantite = quantite;
+
+        }
+
+
+
+        public void Afficher()
+        {
+            Console.WriteLine($" Article : \n type : {typeArticle} \n nom {nom} \n prix {prix}\n quantite {quantite} \n");
+        }
+
+        /// <summary>
+        /// Non utilisée
+        /// TODO : A utiliser et à completer/modifier
+        /// </summary>
+        /// <returns></returns>
+        private int SaisieUtilisateur()
+        {
+            Console.WriteLine("Quelle somme voulez vous retirer ? ");
+            string saisie = Console.ReadLine();
+            int valeur = Convert.ToInt32(saisie);
+
+            return valeur;
+        }
+        public void Ajouter() 
+        {
+            try
+            {
+                Console.WriteLine("Quelle quantite voulez vous ajouter ? ");
+                string saisie = Console.ReadLine();
+                int valeur = Convert.ToInt32(saisie);
+
+                if(valeur>0)
+                {
+                    this.quantite = this.quantite + Convert.ToSingle(valeur);
+                }
+                else
+                {
+                    Console.WriteLine("On ne peut ajouter cette valeur :/ elle est négative ");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Il y a eu une erreur :/ \n {ex.ToString()}");
+            };
+           
+        }
+        /// <summary>
+        /// classe  pour retirer de la quantitée
+        /// TODO: Factoriser dans nv methode private la saisie utilisateur
+        /// </summary>
+        public void Retirer()
+        {
+            Console.WriteLine("Quelle somme voulez vous retirer ? ");
+            string saisie = Console.ReadLine();
+            int valeur = Convert.ToInt32(saisie);
+
+            if (valeur > 0)
+            {
+                //si la saisie est positive
+                this.quantite = quantite - valeur;
+            }
+            else
+            {   // si la saisie est négative
+                this.quantite = quantite + valeur;
+            }
+        }
+    }
+}
